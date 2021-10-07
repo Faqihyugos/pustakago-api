3642637b54321d162ebd8ac213cec112ffef6e39
faqihyugos <faqihzero795@gmail.com>
2021-10-07 15:40:51 +0700

    Http post and JSON gin

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,7 +14,7 @@ func main() {
 	router.GET("/hello", helloHandler)
 	router.GET("/books/:id/:title", booksHandler)
 	router.GET("/query", queryHandler)
-
+	router.POST("/books", postBooksHandler)
 
 
 	router.Run(":8888")
@@ -48,4 +49,27 @@ func queryHandler(c *gin.Context) {
 	price := c.Query("price")
 
 	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
+}
+
+type BookInput struct {
+	Title string `json:"title"`
+	Price int `json:"price"`
+	SubTitle string `json:"sub_title"`
+}
+
+// post books
+func postBooksHandler(c *gin.Context) {
+	// title, price
+	var bookInput BookInput
+	
+	err := c.ShouldBindJSON(&bookInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": bookInput.Title,
+		"price": bookInput.Price,
+		"sub_title": bookInput.SubTitle,
+	})
 }
