dd315d53a58a2e0d81fc37364b02dd5b76c901bc
faqihyugos <faqihzero795@gmail.com>
2021-10-07 16:07:55 +0700

    Show validation

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
@@ -63,9 +64,11 @@ func postBooksHandler(c *gin.Context) {
 	
 	err := c.ShouldBindJSON(&bookInput)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, err)
-		fmt.Println(err)
-		return
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
+			c.JSON(http.StatusBadRequest, errorMessage)
+			return
+		}
 	}
 
 	c.JSON(http.StatusOK, gin.H{
