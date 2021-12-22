e8edb02b0de7a767642a43f53ed40284680b05b8
faqihyugos <faqihzero795@gmail.com>
2021-12-22 15:12:46 +0700

    Auto migration

--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"github.com/Faqihyugos/pustakago-api/book"
 	"github.com/Faqihyugos/pustakago-api/handler"
 	"github.com/gin-gonic/gin"
+	mysql "gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
 )
 
 func main() {
+	// koneksi  db
+	dsn := "root:Secret@tcp(127.0.0.1:3306)/pustaka-api?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Do conection error")
+	}
+	db.AutoMigrate(&book.Book{})
+
+	// router
 	router := gin.Default()
-
 	v1 := router.Group("/v1")
-
 	v1.GET("/", handler.RootHandler)
 	v1.GET("/hello", handler.HelloHandler)
 	v1.GET("/books/:id/:title", handler.BooksHandler)
 	v1.GET("/query", handler.QueryHandler)
 	v1.POST("/books", handler.PostBooksHandler)
-
 
 	router.Run(":8888")
 }
